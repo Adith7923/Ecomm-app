722b294021e64b06bc9dbf6894f176437253109d
Adith7923 <adith7923@gmail.com>
2025-03-23 14:57:34 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Login from "./pages/Login";
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./ProductCard.css";
 export default function ProductCard({ product }) {
