69cde6814a85a0b714b975ffe34010b37a20b5c3
Adith7923 <adith7923@gmail.com>
2025-03-23 15:00:13 +0530

    router config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "react-router-dom": "/node_modules/react-router-dom",
+    },
+  },
+});
